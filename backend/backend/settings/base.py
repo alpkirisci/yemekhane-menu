8dbbc2d71e391096823341fa8827fb0c9da92eff
alpkirisci <salpkirisci@gmail.com>
2024-09-03 01:50:41 +0300

    IFU

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import environ
 import os
 
+
 # Set the project base directory
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -148,10 +149,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, './static')
+
 STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, '../var/www/example.com/static/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, './static'),
+    os.path.join(BASE_DIR, './menu/static')
 ]
 
 
